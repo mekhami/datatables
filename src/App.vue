12a340c33a803840f6d37ef3c407e636fd54fa4b
Lawrence Vanderpool <lawrence.vanderpool@lab7.io>
2018-05-16 22:15:55 -0500

    init commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <data-table :cols="cols" :rows="items"></data-table>
+  </div>
+</template>
+
+<script>
+import DataTable from './components/DataTable'
+
+export default {
+  name: 'App',
+  components: {
+    DataTable
+  },
+  data () {
+    return {
+      cols: [
+        { fieldName: 'animalSpecies', label: 'Species' },
+        { fieldName: 'color', label: 'Color' }
+      ],
+      items: [
+        { animalSpecies: 'pig', color: 'brown' },
+        { animalSpecies: 'cow', color: 'black' },
+        { animalSpecies: 'dog', color: 'white' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <td v-for="col in cols" :key="`${col.fieldName}-head`">
+          {{ col.label }}
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, index) in rows" :key="`row-${index}`">
+        <td v-for="col in cols" :key="`${col.fieldName}-body`">
+          {{ row[col.fieldName] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    cols: {
+      type: Array,
+      required: true
+    },
+    rows: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+
+  }
+}
+</script>
